d1abd0fbdf2ad74029f38f1234cb5390e6467732
plat320 <54256581+plat320@users.noreply.github.com>
2020-06-02 17:17:13 +0900

    AAE

--- testing.py
+++ testing.py
@@ -13,11 +13,12 @@ from torchvision.utils import save_image
 from tensorboardX import SummaryWriter
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
-from sklearn.metrics import roc_auc_score, plot_roc_curve, roc_curve, auc
+from sklearn.metrics import roc_auc_score, plot_roc_curve, roc_curve
+from sklearn.manifold import TSNE
 import time
 import math
 
-import models_VAE as models
+import models_AAE as models
 import load_data
 import utils
 
@@ -27,15 +28,16 @@ def reparameterize(mu, logvar):
     eps = torch.randn_like(std)
     return eps.mul(std).add_(mu)
 
-def testing(model, loader, sample_dir, hidden_size, input_size, epoch, device):
+def testing(encoder, decoder, loader, sample_dir, hidden_size, input_size, epoch, device):
     for (org_image, _) in loader:
         org_image = org_image.to(device)
-        model = model.to(device).eval()
+        encoder = encoder.to(device).eval()
+        decoder = decoder.to(device).eval()
 
-        utils.VAE_get_sample(model, hidden_size, org_image, sample_dir, "abnormal{}".format(epoch), 4, device, input_size)
+        utils.AAE_get_sample(encoder, decoder, hidden_size, org_image, sample_dir, "abnormal{}".format(epoch), 4, device, input_size)
         break
 
-def testing_VAE(model, normal_loader, abnormal_loader, hidden_size, epoch, device, summary):
+def testing_AAE(encoder, normal_loader, abnormal_loader, hidden_size, epoch, device, summary):
     batch_size = 8
 
     fol = ""
@@ -49,11 +51,12 @@ def testing_VAE(model, normal_loader, abnormal_loader, hidden_size, epoch, devic
     with torch.no_grad():
         for (org_image, gt) in normal_loader:
             org_image = org_image.to(device)
-            model.eval()
+            encoder.eval()
 
-            mu, logvar  = model.encoder(org_image)
-            A = torch.abs(torch.sum(mu, dim = (1,2,3))) + torch.abs(torch.sum(logvar, dim = (1,2,3)))
-            A_abs = torch.sum(torch.abs(mu), dim = (1,2,3)) + torch.sum(torch.abs(logvar), dim = (1,2,3))
+            z  = encoder(org_image)
+            # print(z)
+            A = torch.abs(torch.sum(z, dim = (1,2,3)))
+            A_abs = torch.sum(torch.abs(z), dim = (1,2,3))
             # print(torch.mean(mu, dim = (1,2,3)))
             # print(torch.mean(logvar, dim = (1,2,3)))
             normal.extend(A.cpu().detach().numpy())
@@ -61,11 +64,11 @@ def testing_VAE(model, normal_loader, abnormal_loader, hidden_size, epoch, devic
 
     for (org_image, gt) in abnormal_loader:
             org_image = org_image.to(device)
-            model.eval()
+            encoder.eval()
 
-            mu, logvar  = model.encoder(org_image)
-            B = torch.abs(torch.sum(mu, dim = (1,2,3))) + torch.abs(torch.sum(logvar, dim = (1,2,3)))
-            B_abs = torch.sum(torch.abs(mu), dim = (1,2,3)) + torch.sum(torch.abs(logvar), dim = (1,2,3))
+            z  = encoder(org_image)
+            B = torch.abs(torch.sum(z, dim = (1,2,3)))
+            B_abs = torch.sum(torch.abs(z), dim = (1,2,3))
             # print(torch.mean(mu, dim = (1,2,3)))
             # print(torch.mean(logvar, dim = (1,2,3)))
             abnormal.extend(B.cpu().detach().numpy())
@@ -150,7 +153,7 @@ def vec2recon(model, hidden_size, z_mean = None):
 if __name__ == '__main__':
     img_dir = "/media/seonghun/data1/MVTec"
     sample_dir = "sample"
-    save_model = "./VAE_MVTec_model"
+    save_model = "./AAE_MVTec_model"
     save_result = "./result"
     tensorboard_dir = "./tensorboard/AAE"
     img_fol_name = "cable"
@@ -159,7 +162,9 @@ if __name__ == '__main__':
     input_size = int(math.sqrt(input_size))
     batch_size = 8
     hidden_size = [64, 128, 256, 512, 800]
-    save_model = "./VAE_MVTec_model"
+    save_model = "./AAE_MVTec_model"
+
+
 
     check_epoch = "_last"
 
@@ -170,89 +175,97 @@ if __name__ == '__main__':
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+    normal_data = load_data.MVTec_dataloader(image_dir=os.path.join(img_dir,img_fol_name), mode="train_one", in_size = input_size)
+    train_loader = torch.utils.data.DataLoader(normal_data, batch_size=16, shuffle=False, num_workers=2)
 
-    normal_data = load_data.MVTec_dataloader(image_dir=os.path.join(img_dir,img_fol_name), mode=mode, in_size=input_size)
-    train_loader = torch.utils.data.DataLoader(normal_data, batch_size=batch_size, shuffle=True, num_workers=2)
     abnormal_data = load_data.MVTec_dataloader(image_dir=os.path.join(img_dir,img_fol_name), mode="test", in_size=input_size)
-    anomaly_loader = torch.utils.data.DataLoader(abnormal_data, batch_size=batch_size, shuffle=True, num_workers=2)
+    anomaly_loader = torch.utils.data.DataLoader(abnormal_data, batch_size=1, shuffle=True, num_workers=2)
     test_normal_loader = torch.utils.data.DataLoader(load_data.MVTec_dataloader(image_dir = os.path.join(img_dir, img_fol_name), mode="test_normal", in_size = input_size)
-                                                   , batch_size=batch_size, shuffle=True, num_workers=2)
+                                                   , batch_size=1, shuffle=True, num_workers=2)
 
-    model = models.VAE(hidden_size, input_size)
-    model = model.to(device)
-    checkpoint = torch.load(os.path.join(save_model, "test", "checkpoint_best.pth.tar"))
-    model.load_state_dict(checkpoint["model"])
+    encoder = models.AAE_encoder(hidden_size, input_size)
+    decoder = models.AAE_decoder(hidden_size, input_size)
 
-    print("Test normal")
+
+    checkpoint = torch.load(os.path.join(save_model, "test", "checkpoint_last.pth.tar"))
+
+    if hidden_size != checkpoint["hidden_size"]:
+        raise AttributeError("checkpoint's hidden size is not same")
+
+    encoder.load_state_dict(checkpoint['encoder'])
+    decoder.load_state_dict(checkpoint['decoder'])
+
     normal = []
-    abnormal = []
     normal_abs = []
+    normal_euc = []
+
+    abnormal = []
     abnormal_abs = []
+    abnormal_euc = []
+
     with torch.no_grad():
+        for idx, (org_image, _) in enumerate(train_loader):
+            org_image = org_image.to(device)
+            encoder = encoder.to(device).eval()
+
+
+            latent = encoder(org_image)
+
+            if idx == 0:
+                embed = latent
+            else:
+                embed = torch.cat((embed, latent), dim=0)
+
+        print("Test normal")
+
         for (org_image, gt) in test_normal_loader:
             org_image = org_image.to(device)
-            model.eval()
+            encoder = encoder.to(device).eval()
+            decoder = decoder.to(device).eval()
 
-            mu, logvar  = model.encoder(org_image)
-            A = torch.abs(torch.sum(mu, dim = (1,2,3))) + torch.abs(torch.sum(logvar, dim = (1,2,3)))
-            A_abs = torch.sum(torch.abs(mu), dim = (1,2,3)) + torch.sum(torch.abs(logvar), dim = (1,2,3))
+            z  = encoder(org_image)
+            euc = torch.sum(torch.sqrt(torch.sum((embed-z)**2, dim=1))).item()
+
+
+            A = torch.abs(torch.sum(z, dim = (1,2,3)))
+            A_abs = torch.sum(torch.abs(z), dim = (1,2,3))
             # print(torch.mean(mu, dim = (1,2,3)))
             # print(torch.mean(logvar, dim = (1,2,3)))
             normal.extend(A.cpu().detach().numpy())
             normal_abs.extend(A_abs.cpu().detach().numpy())
-        print(normal_abs)
+            normal_euc.append(euc)
 
         for (org_image, gt) in anomaly_loader:
             org_image = org_image.to(device)
-            model.eval()
+            encoder = encoder.to(device).eval()
+            decoder = decoder.to(device).eval()
 
-            mu, logvar  = model.encoder(org_image)
-            B = torch.abs(torch.sum(mu, dim = (1,2,3))) + torch.abs(torch.sum(logvar, dim = (1,2,3)))
-            B_abs = torch.sum(torch.abs(mu), dim = (1,2,3)) + torch.sum(torch.abs(logvar), dim = (1,2,3))
+            z  = encoder(org_image)
+            B = torch.abs(torch.sum(z, dim = (1,2,3)))
+            B_abs = torch.sum(torch.abs(z), dim = (1,2,3))
+            euc = torch.sum(torch.sqrt(torch.sum((embed-z)**2, dim=1))).item()
             # print(torch.mean(mu, dim = (1,2,3)))
             # print(torch.mean(logvar, dim = (1,2,3)))
             abnormal.extend(B.cpu().detach().numpy())
             abnormal_abs.extend(B_abs.cpu().detach().numpy())
-    print(abnormal_abs)
+            abnormal_euc.append(euc)
+
+        normal_euc = np.array(normal_euc)
+        abnormal_euc = np.array(abnormal_euc)
+        print(normal_euc.mean())
+        print(abnormal_euc.mean())
+
 
     normal_label = np.zeros_like(normal)
     abnormal_label = np.ones_like(abnormal)
     print("normal mean : {:.4f}, abnormal mean : {:.4f}, normal abs : {:.4f}, abnormal abs : {:.4f}".format(statistics.mean(normal), statistics.mean(abnormal), statistics.mean(normal_abs), statistics.mean(abnormal_abs)))
     score = roc_auc_score(np.hstack((normal_label, abnormal_label)), np.hstack((normal, abnormal)))
     abs_score = roc_auc_score(np.hstack((normal_label, abnormal_label)), np.hstack((normal_abs, abnormal_abs)))
-
-    result = dict()
-    grid_num = 40
-    a = np.zeros(grid_num)
-    TP = np.zeros(grid_num)
-    FN = np.zeros(grid_num)
-    TN = np.zeros(grid_num)
-    FP = np.zeros(grid_num)
-    TPR = np.zeros(grid_num)
-    FPR = np.zeros(grid_num)
-    save_num = 0
-
-    # print("discrete accuracy")
-    # for num in range(grid_num):
-    #     b = num / grid_num
-    #     TP[num] = np.sum(normal <= b) / normal.size
-    #     TN[num] = np.sum(abnormal <= b) / abnormal.size
-    #     FN[num] = np.sum(normal > b) / normal.size
-    #     FP[num] = np.sum(abnormal > b) / abnormal.size
-    #     a[num] = b
-    #     TPR[num] = TP[num] / (TP[num] + FN[num])
-    #     FPR[num] = 1 - FP[num] / (FP[num] + TN[num])
-    #     print("threshold : {:.2f}, TP : {:.2f}, FP : {:.2f}, TN : {:.2f}, FP : {:.2f}, TPR : {:.2f}, FPR : {:.2f}"
-    #           .format(b, TP[num], FP[num], TN[num], FN[num], TPR[num], FPR[num]))
-
-    print("AUROC score : ")
-    print(roc_auc_score(np.hstack((normal_label, abnormal_label)), np.hstack((normal_abs, abnormal_abs))))
-
-    # normal_abs = (normal_abs-min(normal_abs))/(max(normal_abs) - min(normal_abs))
-    # abnormal_abs = (abnormal_abs-min(abnormal_abs))/(max(abnormal_abs) - min(abnormal_abs))
+    euc_score = roc_auc_score(np.hstack((normal_label, abnormal_label)), np.hstack((normal_euc, abnormal_euc)))
     fpr, tpr, thres = roc_curve(np.hstack((normal_label, abnormal_label)), np.hstack((normal_abs, abnormal_abs)), pos_label=1)
-    print(auc(fpr, tpr))
-
+    print(fpr)
+    print(tpr)
+    print("score : {:.4f}, abs_score : {:.4f}, euc_score".format(score, abs_score, euc_score))
     plt.figure(figsize=(10, 6))
     plt.subplot(121)
     plt.plot(fpr, tpr)
@@ -266,14 +279,14 @@ if __name__ == '__main__':
 
     # plt.show()
 
-    plt.figure(figsize=(10, 12))
+    plt.figure(figsize=(20, 12))
     # plt.subplot(121)
     # plt.plot(a, TP)
     # plt.plot(a, FN)
     # plt.plot(a, TN)
     # plt.plot(a, FP)
     # plt.legend(["TP", "FN", "TN", "FP"])
-    bins = np.linspace(600,2500, 100)
+    bins = np.linspace(0,1,30)
     #
     plt.subplot(122)
     plt.hist([normal_abs, abnormal_abs], bins, label=['normal', 'abnormal'])
